5a6d338d597e2f9e8dec05077d8d19206dbffd85
Manuel Palanco <Trystan4861@gmail.com>
2025-03-31 09:54:12 +0200

    refactor token

--- includes/actions/get/getIdUsuario.inc.php
+++ includes/actions/get/getIdUsuario.inc.php
@@ -1,11 +1,12 @@
 <?php
   $token=null;
-  if (isset($_MySERVER['HTTP_AUTHORIZATION']) && strpos($_MySERVER['HTTP_AUTHORIZATION'], 'Bearer ') === 0){
+  if (isset($data["token"])) {
+    $token=$data["token"];
+  }
+  elseif (isset($_MySERVER['HTTP_AUTHORIZATION']) && strpos($_MySERVER['HTTP_AUTHORIZATION'], 'Bearer ') === 0){
     $token = substr($_MySERVER['HTTP_AUTHORIZATION'], 7);
   }elseif (isset($_MySERVER['HTTP_TOKEN'])) {
     $token=$_MySERVER['HTTP_TOKEN'];
-  }elseif (isset($data["token"])) {
-    $token=$data["token"];
   }
   if($token==null)
   {
@@ -13,7 +14,12 @@
   }
   else
   {
-    $user=new Usuario($token);
+    // Si tenemos fingerid en los datos, lo pasamos junto con el token
+    if(isset($data["finger_id"]))
+    {
+      $token=["token"=>"$token","finger_id"=>$data["finger_id"]];
+    }
+    $user = new Usuario($token);
     if ($user->getLastResult())
     {
       $id_usuario=$user->getIdUsuario();
